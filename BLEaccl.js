e4fdae7c9f9e04a5296d2687c0323ee19a6d9220
greg <gregcj@hotmail.co.uk>
2018-03-29 21:25:14 +0100

    Get xyz on sever not working

--- BLEaccl.js
+++ BLEaccl.js
@@ -1,10 +1,13 @@
 //Code adapted from Noble examples see: https://github.com/noble/noble/blob/master/examples/advertisement-discovery.js
 var noble = require('noble');
+var util = require('utility');
 
 console.log("Searching for microbit.");
 var regex = new RegExp('/^microbit\(([a-z]*)\)/');
 
-//State of local bluetooth device changed.
+var xyz = [];
+function getAccl(){
+    //State of local bluetooth device changed.
 noble.on('stateChange', function (state)
 {
     if (state === 'poweredOn')
@@ -46,31 +49,63 @@ noble.on('stateChange', function (state)
             peripheral.connect(function (error) {
                 console.log("Connected to microbit!");
                 //List of services taken from BLE spec.
-                    //Acclerometer service UUIDs.
-                    var accl = "e95d0753251d470aa062fa1922dfa9a8";
-                    //Accelerometer data.
-                    var acclData = "e95dca4b251d470aa062fa1922dfa9a8";
-                    //Accelerometer period is how often it is queried for data.
-                    var acclPer = "e95dfb24251d470aa062fa1922dfa9a8";
-                    //Magnetometer service.
-                    var magne = "E95DF2D8251D470AA062FA1922DFA9A8";
-                    //Magnetometer data.
-                    var magneData = "";
+                //Acclerometer service UUIDs.
+                var accl = "e95d0753251d470aa062fa1922dfa9a8";
+                //Accelerometer data.
+                var acclData = "e95dca4b251d470aa062fa1922dfa9a8";
+                //Accelerometer period is how often it is queried for data.
+                var acclPer = "e95dfb24251d470aa062fa1922dfa9a8";
+                //Magnetometer service.
+                var magne = "E95DF2D8251D470AA062FA1922DFA9A8";
+                //Magnetometer data.
+                var magneData = "";
                 //Get services, UUIDs for microbit services taken from lancaster uni BLE spec.
                 peripheral.discoverServices([], function (error, services) {
                     console.log("Trying to get services");
                     
-                    console.log(services);
+                    //console.log(services);
+                    //Iterate through list of services, this could be changed to for each.
                     for(var i=0, len = services.length; i < len; i++)
                     {
-                        console.log(services[i].uuid);
+                        //console.log(services[i].uuid);
                         if(services[i].uuid == accl)
                         {
                             console.log("Found accl");
                             var acclServ = services[i];
+                            //Get characteristics for this service.
                             acclServ.discoverCharacteristics([], function (error, chars) {
+
                                 chars.forEach(function (chars) {
-                                    console.log('Char uuid:',chars.uuid);
+                                    //console.log('Char uuid:', chars.uuid);
+                                    if (chars.uuid == acclData)
+                                    {
+                                        var acclDataChar = chars;
+                                        console.log("Got the data char");
+                                        acclDataChar.read(function (error, data) {
+                                            //console.log("Raw data:", data);
+                                            //Raw data from accelerometer.
+                                            var rawData = Array.prototype.slice.call(data, 0);
+                                            var rawX = new Int16Array();
+                                            rawX = rawData.slice(0, 1);
+                                            var rawY = new Int16Array();
+                                            rawY = rawData.slice(2, 3);
+                                            var rawZ = new Int16Array();
+                                            rawZ = rawData.slice(4, 5);
+                                            //console.log("Raw X val",rawX);
+                                            //console.log("Raw y val",rawY);
+                                            //console.log("Raw Z val", rawZ);
+                                            var datX = rawX / 1000;
+                                            var datY = rawY / 1000;
+                                            var datZ = rawZ / 1000;
+                                            console.log("X axis:", datX);
+                                            console.log("Y axis:", datY);
+                                            console.log("Z axis:", datZ);
+                                            xyz[0] = datX;
+                                            xyz[1] = datY;
+                                            xyz[2] = datZ;
+                                        })
+                                    }
+                                   
                                 })
                             });
                         }
@@ -80,4 +115,7 @@ noble.on('stateChange', function (state)
         }
         console.log();
     });
+}
 
+module.exports.getAccl = getAccl;
+module.exports.xyz = xyz;
--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-//CODE ORIGINALLY FROM https://www.youtube.com/watch?v=U8XF6AFGqlc
-const http = require('http');
+//Using express for routing.
+const express = require('express');
+const app = express();
+
+
 //Bluetooth module.
 const noble = require('noble');
-//File handeling module.
-const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const microAccl = require('./BLEaccl.js');
 
-//Read the html file then start the server with it.
-fs.readFile('index.html', (error, html) => {
-    //If there is an error throw it.
-    if (error) {
-        throw error;
-    }
-
-    //Request + response are passsed into the server via arrow function.
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write(html);
-        res.end();
-    });
-
-    //Sever is now running.
-    server.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+//Respond too app index page data.
+app.get('/', (req, res) => {
+    res.sendFile('D:\\programs\\uni\\IOT\\Challenge3\\index.html');
 });
-
-
+const test = microAccl.getAccl();
+const xyz = microAccl.xyz;
+console.log(xyz);
+//Serve the static public folder so it can be used.
+app.use(express.static('public'));
+//Start Server.
+app.listen(3000, () => console.log('Sever running on port 3000'));
 
 
