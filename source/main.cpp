0f79fdb2edf6c606ea567cec1e5cc154a2c812c4
greg <gregcj@hotmail.co.uk>
2018-03-30 20:10:38 +0100

    Started work on Microbit
    Can connect to BLE, and has accelerometer service running. Started working on UART service.

---
+++ source/main.cpp
@@ -0,0 +1,46 @@
+#include "MicroBit.h"
+#include "MicroBitUARTService.h"
+
+MicroBit uBit;
+MicroBitUARTService *uart;
+
+//PROTOTYPES
+void conn(MicroBitEvent e);
+void dis(MicroBitEvent e);
+
+int main()
+{
+  uBit.init();
+  new MicroBitAccelerometerService(*uBit.ble, uBit.accelerometer);
+  //new MicroBitMagnetometerService(*uBit.ble, uBit.compass);
+
+  //Listen for connection.
+  uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_CONNECTED, conn);
+  //Listen for disconnection.
+  uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_DISCONNECTED, dis);
+
+  //Button listeners.
+  MicroBitButton buttonA(MICROBIT_PIN_BUTTON_A, MICROBIT_ID_BUTTON_A);
+  MicroBitButton buttonB(MICROBIT_PIN_BUTTON_B, MICROBIT_ID_BUTTON_B);
+
+  //Message loop, get input from Microbit till end of message (EOM).
+  bool msg = false;
+  while(msg == false)
+  {
+
+  }
+  release_fiber();
+}
+
+//FUNCTIONS
+//Connected via BLE to central.
+void conn(MicroBitEvent e)
+{
+  uBit.display.print("C");
+}
+
+//Disconnected detected on BLE.
+void dis(MicroBitEvent e)
+{
+  uBit.display.print("D");
+}
