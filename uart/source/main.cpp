94c02fcce41b76f3d9df338bfb73c84830a8b4aa
greg <gregcj@hotmail.co.uk>
2018-04-03 22:31:31 +0100

    Can send data over uart
    Started to make packet structure in notes.

--- uart/source/main.cpp
+++ uart/source/main.cpp
@@ -7,35 +7,59 @@ MicroBitUARTService *uart;
 //PROTOTYPES
 void conn(MicroBitEvent e);
 void dis(MicroBitEvent e);
-void scroll(MicroBitEvent e);
-
-//Need to the global for displaying character for user input.
-
 
 //Button listeners.
 MicroBitButton buttonA(MICROBIT_PIN_BUTTON_A, MICROBIT_ID_BUTTON_A);
 MicroBitButton buttonB(MICROBIT_PIN_BUTTON_B, MICROBIT_ID_BUTTON_B);
 
+
 int main()
 {
   uBit.init();
-   uart = new MicroBitUARTService(*uBit.ble, 32, 32);
+  uart = new MicroBitUARTService(*uBit.ble, 32, 32);
   //Listen for connection.
   uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_CONNECTED, conn);
   //Listen for disconnection.
   uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_DISCONNECTED, dis);
+
+  //Message string, must be terminated by | to send.
+  ManagedString msgString = 0;
+  //65 default value for display character, this is the ascii value for A.
   char inChar = 65;
 
   //Message loop, broken when eom '|' (124) is entered.
   bool msg = false;
   bool lock = false;
+
   while(msg == false)
   {
+    //Get user input, the displayed character is added to the message string,
+    //by pressing both buttons at once.
+    //Boolean lock is used to prevent inconsistant behaviour with input.
+    while(buttonA.isPressed() && buttonB.isPressed())
+    {
+      if(!lock)
+      {
+        uBit.display.print("+");
+        msgString = msgString + inChar;
+        uBit.sleep(500);
+        lock = true;
+      }
+    }
+    lock = false;
     while(buttonA.isPressed())
     {
+      //End message;
+      while(buttonB.isPressed())
+      {
+        uBit.display.print("|");
+        uart->send("1ES");
+        lock = true;
+      }
       //Increment once per click, by use of the lock.
       if(!lock)
       {
+
         //Wrap the value around character set we want [A-Z].
         if(inChar == 90)
         {
